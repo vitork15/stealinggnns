648630dc32349508def02fc42ab2c7108a6b8b01
Vitor Manoel <vitorearthur15@hotmail.com>
2026-06-08 19:24:40 -0300

    prototipo da pipeline transdutiva

---
+++ main_transductive.py
@@ -0,0 +1,202 @@
+import numpy as np
+import torch
+import random
+from torch_geometric.nn import GIN, GCN, GAT, GraphSAGE, GCNConv
+from torch_geometric.datasets import Reddit, Planetoid, Amazon, Flickr
+from torch_geometric.loader import DataLoader
+from torch.nn import MSELoss, CrossEntropyLoss, Linear, ReLU, BatchNorm1d, Dropout, Sequential, Sigmoid
+from torch_geometric.transforms import NormalizeFeatures
+import torch.nn.functional as F
+from tqdm import tqdm
+from utils.lagraph import LaGraphNodeLoss, DecoderGCN, DecoderMLP, node_mask, Encoder
+import matplotlib.pyplot as plt
+import matplotlib as mlp
+from sklearn.metrics import classification_report, pairwise_distances_argmin_min
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+
+def select_indices(X, k):
+    X_norm = StandardScaler().fit_transform(X)
+
+    kmeans = KMeans(
+        n_clusters=k,
+        n_init=5
+    ).fit(X_norm)
+
+    closest, _ = pairwise_distances_argmin_min(
+        kmeans.cluster_centers_,
+        X_norm
+    )
+
+    return closest
+
+class HeadMLP(torch.nn.Module):
+    def __init__(self, dim_in=32, dim_out=32):
+        super(HeadMLP, self).__init__()
+        
+        self.fc = Sequential(
+            Linear(dim_in, dim_out),
+            #Sigmoid()
+        )
+
+    def forward(self, x):
+        return self.fc(x)
+
+def main():
+    # adicional: implementar watermark https://arxiv.org/pdf/2110.11024 TBD
+
+    tqdm_off = False
+    
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    
+    victim = 'gat'
+    surrogate_conv = 'gcn'
+    surrogate_act = 'relu'
+    
+    seed = 451
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    
+    batch_size = 32
+    victim_hidden_size = 256
+    embedding_hidden_size = 256
+    
+    victim_epochs = 30
+    ssl_epochs = 30
+    surrogate_epochs = 100
+    
+    query_limit = 10
+    
+    # carregar o dataset
+    dataset = Planetoid(root='./datasets/Planetoid', name='Cora')
+    
+    dataset.to(device)
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    
+    if victim == 'gat':
+        victim_model = GAT(in_channels=dataset.num_features, hidden_channels=victim_hidden_size, out_channels=dataset.num_classes, num_layers=2, act='relu', dropout=0.5).to(device)
+    elif victim == 'gcn':
+        victim_model = GCN(in_channels=dataset.num_features, hidden_channels=victim_hidden_size, out_channels=dataset.num_classes, num_layers=2, act='relu', dropout=0.5).to(device)
+        
+    victim_optimizer = torch.optim.Adam(victim_model.parameters(), lr=1e-3)
+    
+    # treinar o classificador da vitima nos embeddings do dataset rotulado
+    for _ in tqdm(range(victim_epochs), desc='Treino Vitima', disable=tqdm_off):
+        for batch in loader:
+            victim_model.train()
+
+            victim_optimizer.zero_grad()
+            
+            pred = victim_model(batch.x, batch.edge_index)
+
+            loss = CrossEntropyLoss()(pred[batch.train_mask], batch.y[batch.train_mask])
+            
+            loss.backward()
+            
+            victim_optimizer.step()
+            
+    victim_model.eval()
+    
+    y_true = []
+    y_pred = []
+
+    for batch in loader:      
+        pred = victim_model(batch.x, batch.edge_index)
+        pred = torch.argmax(pred, dim=-1)
+        
+        y_pred.append(pred[batch.test_mask].cpu())
+        y_true.append(batch.y[batch.test_mask].cpu())
+        
+    y_pred = torch.cat(y_pred).numpy()
+    y_true = torch.cat(y_true).numpy()
+            
+    print(classification_report(y_true, y_pred, digits=3, zero_division=0))
+    
+    #treinar o embedding do surrogate
+
+    encoder = Encoder(dim_in=dataset.num_features, dim_out=embedding_hidden_size, num_layers=2, conv=surrogate_conv, act=surrogate_act)
+    encoder.to(device)
+    
+    decoder = DecoderMLP(embedding_hidden_size, dim_out=dataset.num_features, num_layers=1)
+    decoder.to(device)
+    
+    optimizer_encoder = torch.optim.Adam(encoder.parameters(), lr=1e-3)
+    optimizer_decoder = torch.optim.Adam(decoder.parameters(), lr=1e-3)
+    
+    lagraph_loss = LaGraphNodeLoss()
+
+    for _ in tqdm(range(ssl_epochs), desc='Treino SSL', disable=tqdm_off):
+        for batch in loader:
+            encoder.train()
+            decoder.train()
+            
+            optimizer_encoder.zero_grad()
+            optimizer_decoder.zero_grad()
+            
+            noisy_node_feature, mask = node_mask(batch.x, batch.train_mask, std=0.5, ratio=0.05, device=device) 
+            
+            embedding_original = encoder(batch.x, batch.edge_index)
+            embedding_noisy = encoder(noisy_node_feature, batch.edge_index)
+            
+            reconstructed = decoder(embedding_original)
+
+            loss = lagraph_loss(batch.x[batch.train_mask], reconstructed[batch.train_mask], embedding_original[batch.train_mask], embedding_noisy[batch.train_mask], mask[batch.train_mask]) 
+            loss.backward()
+            
+            optimizer_encoder.step()
+            optimizer_decoder.step()
+            
+    encoder.eval()
+
+    surrogate_head = HeadMLP(embedding_hidden_size, dataset.num_classes).to(device)
+    surrogate_optimizer = torch.optim.Adam(surrogate_head.parameters(), lr=1e-2)
+    
+    # escolher os nós que vao ser usados pelo adversario por k-means no dataset sem rotulos
+    selected_idx = select_indices(encoder(dataset[0].x, dataset[0].edge_index).cpu().detach(), query_limit)
+    selected_idx = torch.sort(torch.tensor(selected_idx)).values
+    
+    # usar a vitima para encontrar os rotulos dos embeddings
+    victim_pred = victim_model(dataset[0].x, dataset[0].edge_index)
+    victim_pred = torch.argmax(victim_pred, dim=-1)
+    
+    # treinar o classificador do surrogate nos rotulos obtidos da vitima
+    for _ in tqdm(range(surrogate_epochs), desc='Treino Classificador', disable=tqdm_off):
+        for batch in loader:
+            surrogate_head.train()
+
+            victim_optimizer.zero_grad()
+            
+            embedding = encoder(batch.x, batch.edge_index)
+
+            pred = surrogate_head(embedding[selected_idx])
+            loss = CrossEntropyLoss()(pred, victim_pred[selected_idx])
+            
+            loss.backward()
+            
+            surrogate_optimizer.step()
+            
+    surrogate_head.eval()
+    
+    y_true = []
+    y_pred = []
+
+    for batch in loader:    
+        
+        embedding = encoder(batch.x, batch.edge_index)
+          
+        pred = surrogate_head(embedding[batch.test_mask])
+        pred = torch.argmax(pred, dim=-1)
+        
+        y_pred.append(pred.cpu())
+        y_true.append(batch.y[batch.test_mask].cpu())
+        
+    y_pred = torch.cat(y_pred).numpy()
+    y_true = torch.cat(y_true).numpy()
+            
+    print(classification_report(y_true, y_pred, digits=3, zero_division=0))
+    
+    
+if __name__ == "__main__":
+    main()
---
+++ utils/lagraph.py
@@ -0,0 +1,167 @@
+import torch
+import torch.nn as nn
+from torch.nn import Linear, ReLU, PReLU, BatchNorm1d, Dropout
+from torch_geometric.nn import GCNConv, GATConv, SAGEConv, GINConv
+from torch.nn.functional import relu, prelu, leaky_relu
+import numpy as np
+
+class Encoder(torch.nn.Module):
+    def __init__(self, dim_in=32, dim_out=32, num_layers=5, act='relu', conv='gcn'):
+        super(Encoder, self).__init__()
+
+        self.num_layers = num_layers
+        
+        self.conv_list = torch.nn.ModuleList()
+        self.batchnorm_list = torch.nn.ModuleList()
+        
+        activations = {
+            'relu': relu,
+            'prelu': prelu,
+            'leakyrelu': leaky_relu
+        }
+        
+        conv_layers = {
+            'gcn': GCNConv,
+            'gat': GATConv,
+            'sage': SAGEConv,
+            'gin': GINConv
+        }
+        
+        self.conv = conv_layers[conv]  
+        self.act = activations[act]
+        
+        for layer_idx in range(num_layers):
+            start_dim = dim_out if layer_idx else dim_in
+            self.conv_list.append(self.conv(start_dim, dim_out))
+            self.batchnorm_list.append(BatchNorm1d(dim_out))
+
+    def forward(self, x, edge_index):
+        for i in range(self.num_layers):
+            x = self.conv_list[i](x, edge_index)
+            x = self.act(x)
+            x = self.batchnorm_list[i](x)
+
+        return x
+
+class DecoderGCN(torch.nn.Module):
+    def __init__(self, dim_in=32, dim_out=32, num_layers=5):
+        super(DecoderGCN, self).__init__()
+        
+        layers = nn.Sequential(
+            GCNConv(dim_in, dim_in),
+            PReLU(),
+            BatchNorm1d(dim_in)
+        )
+        
+        self.network = nn.Sequential(
+            *[layers for i in range(num_layers-1)],
+            GCNConv(dim_in, dim_out),
+            PReLU()
+        )
+
+    def forward(self, x):
+        return self.network(x)
+
+
+class DecoderMLP(torch.nn.Module):
+    def __init__(self, dim_in=32, dim_out=32, num_layers=5, dropout=0.5):
+        super(DecoderMLP, self).__init__()
+        
+        layers = nn.Sequential(
+            Linear(dim_in, dim_in),
+            BatchNorm1d(dim_in),
+            ReLU(),
+            Dropout(dropout)
+        )
+        
+        self.fc = nn.Sequential(
+            *[layers for i in range(num_layers-1)],
+            Linear(dim_in, dim_out),
+            ReLU()
+        )
+
+    def forward(self, x):
+        return self.fc(x)
+    
+class LaGraphNodeLoss(torch.nn.Module):
+    '''
+    _Node-level loss_ como definido pelo artigo [Self-Supervised Representation Learning via Latent Graph Prediction](https://arxiv.org/pdf/2202.08333)
+    '''
+    
+    def __init__(self):
+        super(LaGraphNodeLoss, self).__init__()
+
+    def forward(self, original, reconstructed, representation_orig, representation_rec, mask, alpha=2):
+        loss_reconstruction = nn.MSELoss()(original, reconstructed)
+        loss_invariance = nn.MSELoss(reduction='none')(representation_orig, representation_rec)
+        loss_invariance = torch.sqrt(torch.sum(loss_invariance * mask[:,None]) / torch.sum(mask).clamp(min=1))
+        loss = loss_reconstruction + alpha * loss_invariance
+        
+        return loss
+    
+def node_mask(x, idx_mask, std, ratio, device='cpu'):
+    '''
+    Recebe um mini-batch de grafos (formato PyG) e retorna o grafo com uma chance de ``ratio`` individual de um nó ser substituído por ruído com média 0 e desvio padrão ``std``.
+    
+    Parameters
+    ----------
+    x : ndarray da forma (n_node, n_feat)
+        Mini-batch de grafos
+    std: float
+        Desvio padrão do ruído
+    ratio: float
+        Chance de um nó ser substituído por ruído
+
+    Returns
+    -------
+    x : ndarray da forma (n_node, n_feat)
+        Mini-batch com ruído inserido
+    mask : ndarray da forma (n_node,)
+        Máscaras com nós ruidosos para cada grafo
+    '''
+    
+    x_masked = x.clone()
+
+    node_num, feat_dim = x.size()
+
+    idx = torch.where(idx_mask)[0]
+
+    mask_size = int(ratio * len(idx))
+
+    perm = torch.randperm(len(idx), device=x.device)
+    mask_idx = idx[perm[:mask_size]]
+
+    noise = torch.normal(
+        mean=0.0,
+        std=std,
+        size=(node_num, feat_dim),
+        device=x.device
+    )
+
+    mask = torch.zeros(node_num, dtype=torch.bool, device=x.device)
+    mask[mask_idx] = True
+
+    x_masked[mask] = noise[mask]
+
+    return x_masked, mask
+    
+    x_masked = x.clone()
+
+    node_num, feat_dim = x[idx_mask].size()
+    mask_size = int(ratio * node_num)
+
+    mask_idx = torch.randperm(node_num, device=device)[:mask_size]
+
+    noise = torch.normal(
+        mean=0.0,
+        std=std,
+        size=(node_num, feat_dim),
+        device=device
+    )
+
+    mask = torch.zeros(node_num, dtype=torch.bool, device=device)
+    mask[mask_idx] = 1
+
+    x_masked[mask] = noise[mask]
+
+    return x_masked, mask
